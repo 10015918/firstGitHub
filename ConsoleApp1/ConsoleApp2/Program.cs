d2a4aacb11522cb300f018c5b138da7c2a644baf
Johan Vermaak <10015918@student.toiohomai.ac.nz>
2018-07-25 09:39:53 +1200

    first push

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;//a whole number
+            string input;
+            float num2;
+            decimal num3;
+            double num4;
+
+
+            for (var i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Hello World");
+            }
+            Console.ReadLine();
+        }
+    }
+}
